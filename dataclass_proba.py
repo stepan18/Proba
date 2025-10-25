d4c520488916a7761cdaf7ad513badc68a161e31
ÐStepan Suvorov <presnyakov-1825@mail.ru>
2025-10-25 20:53:28 +0300

    Лучший класс

---
+++ dataclass_proba.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    user_id: int
+    name: str
+    age: int
+    email: str
+
+def get_user_info(user: User) -> str:
+    return f'Возраст пользователя {user.name} - {user.age}, ' \
+           f'а email - {user.email}'
+
+
+user_1 = User(42, 'Vasiliy', 23, 'vasya_pupkin@pochta.ru')
+print(get_user_info(user_1))
